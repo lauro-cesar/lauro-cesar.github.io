4ed6c84080b2c7c6a3cf76078a071a74de87c984
Lauro C Oliveira <lauro@hostcert.com.br>
2018-03-10 20:11:41 +0000

    Added global controller specs

--- src/workspace/packages/local/omniCore/src/OmniCoreController.js
+++ src/workspace/packages/local/omniCore/src/OmniCoreController.js
@@ -1,7 +1,16 @@
 Ext.define('OmniEshops.core.OmniCoreController', {
     extend: 'Ext.app.ViewController',
     alias: 'controller.omniCoreController',
+    omniRoutes:[
+        {
+            'home':'onHome'
+        }
+    ],
     init:function(){
         console.log("Core Controller started");
+        console.log(this.getRoutes());
+    },
+    onHome: function () {
+        console.log("Home...");
     }
 });
