80fe12c2c8d8db69be22af5895aeb24a60cd4f81
Lauro C Oliveira <lauro@hostcert.com.br>
2018-03-16 13:52:45 +0000

    Added BoardGame Creator app

---
+++ src/workspace/apps/BoardGameCreator/app/view/main/MainPanel.js
@@ -0,0 +1,9 @@
+Ext.define('OmniManager.view.main.MainPanel', {
+    extend: 'Ext.panel.Panel',
+    xtype: 'mainPanel',
+    alias:'widget.mainPanel',
+    mixins: [
+        'Ext.mixin.Responsive'
+    ],
+    html:'Empty Panel'
+});
